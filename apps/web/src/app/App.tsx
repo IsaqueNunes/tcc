8cd405f0ee633ef42b6286f1d12c1231b1d67be0
Rafael <rafael_todescato@hotmail.com>
2022-06-11 01:14:08 +0000

    fix: testing commits

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,8 +1,12 @@
+// import styled from 'styled-components';
+// import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import RoutesNavigator from './routes';
 
 export default function App() {
   return (
+
     <RoutesNavigator />
+
   );
 }
