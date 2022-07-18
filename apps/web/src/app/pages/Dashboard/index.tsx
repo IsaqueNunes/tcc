92e7a8424a0c1fdbb7f25c3e7734204554331997
Rafael <rafael_todescato@hotmail.com>
2022-07-18 15:35:51 +0000

    feat(web): added dashboard page

--- apps/web/src/app/pages/Dashboard/index.tsx
+++ apps/web/src/app/pages/Dashboard/index.tsx
@@ -1,8 +1,62 @@
+import { useState } from 'react';
 import Card from '../../components/Card';
 import Header from '../../components/Header';
+import LineChart from '../../components/LineChart';
+import PieChart from '../../components/PieChart';
 import './dashboard.css';
 
+export const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
+
 export default function Dashboard() {
+  const [userData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: 'Users Gained',
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          'rgba(75,192,192,1)',
+          '#ecf0f1',
+          '#50AF95',
+          '#f3ba2f',
+          '#2a71d0',
+        ],
+        borderColor: 'black',
+        borderWidth: 2,
+      },
+    ],
+  });
   return (
     <>
       <Header typeOfHeader="admin" />
@@ -35,6 +89,14 @@ export default function Dashboard() {
             hexColorStatus="#FAE52D"
             nameStatus="Em análise"
           />
+        </div>
+      </div>
+      <div className="charts-container">
+        <div style={{ width: 700 }}>
+          <LineChart chartData={userData} />
+        </div>
+        <div style={{ width: 400 }}>
+          <PieChart chartData={userData} />
         </div>
       </div>
     </>
