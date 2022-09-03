7fcefefd084d4ae5628c6d5003a24b55888189b5
Rafael Veiga <rafael_todescato@hotmail.com>
2022-09-03 08:58:25 -0400

    feat(web): added user information inside top message

--- apps/web/src/app/pages/Chat/index.tsx
+++ apps/web/src/app/pages/Chat/index.tsx
@@ -1,4 +1,4 @@
-import { Message as MessageType, Prisma } from '@prisma/client';
+import { Message as MessageType, Prisma, User } from '@prisma/client';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Button from '../../components/Button';
@@ -6,7 +6,7 @@ import Header from '../../components/Header';
 import Message from '../../components/Message';
 import './chat.css';
 
-interface TicketMessage {
+type TicketMessage = {
   id: number
   title: string
   content: string
@@ -15,7 +15,8 @@ interface TicketMessage {
   closedAt: Date | null
   userId: string
   Message: MessageType[];
-}
+  user: User;
+};
 
 export default function Chat() {
   const { id } = useParams();
@@ -32,7 +33,7 @@ export default function Chat() {
   const AddMessage = () => {
     const message: Prisma.MessageUncheckedCreateInput = {
       content: description,
-      userId: 'c9668c52-40ce-4fd9-bc6d-555c6e8dbce9',
+      userId: '04b3109e-2b82-49fc-b4f4-0f94c5148907',
       repliedMessageId: ticket?.Message.length === 0 ? null
         : ticket?.Message[(ticket?.Message?.length || 1) - 1].id,
       ticketId: Number(id),
@@ -56,8 +57,8 @@ export default function Chat() {
               {' '}
               {ticket?.title}
             </h1>
-            <select name="status" id="status-select">
-              <option value="" disabled selected>Selecione um status</option>
+            <select name="status" id="status-select" aria-label="status-select">
+              <option disabled defaultValue="">Selecione um status</option>
               <option value="1">teste</option>
             </select>
           </div>
@@ -65,8 +66,8 @@ export default function Chat() {
             <div className="first-message">
               <div className="top-content">
                 <div className="name-email">
-                  <h3><strong>Felipe Araújo</strong></h3>
-                  <span>felipe.araujo@estudante.ifms.edu.br</span>
+                  <h3><strong>{ticket?.user?.name}</strong></h3>
+                  <span>{ticket?.user?.email}</span>
                 </div>
                 <h5 className="no-wrap">{ticket?.createdAt?.toString()}</h5>
               </div>
@@ -78,6 +79,7 @@ export default function Chat() {
             </div>
             {ticket?.Message.map((message: MessageType) => (
               <Message
+                key={message.id}
                 messageContent={message.content}
                 messageCreatedDate={message.time.toString()}
               />
@@ -87,6 +89,7 @@ export default function Chat() {
             <textarea
               name="message"
               id="message"
+              aria-label="message-send-content"
               onChange={(event) => setDescription(event.target.value)}
             />
             <div className="button-add-message-container">
