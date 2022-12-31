251c443e49ca5dbde3c69e6d30e81b87ab273195
Rafael Veiga <rafael_todescato@hotmail.com>
2022-12-31 13:21:00 -0400

    feat(mobile): added new project

---
+++ apps/mobile/src/app/App.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export const App = () => {
+
+  return (
+    <View>
+      <Text>teste</Text>
+    </View>
+  );
+};
+
+export default App;
